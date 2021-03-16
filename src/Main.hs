c8e76d43514269caacdde7b427bbfb0b6fdba425
tweek <vallkhale@gmail.com>
2021-03-16 19:57:25 +0300

    temp

--- src/Main.hs
+++ src/Main.hs
@@ -13,13 +13,14 @@ main =
     print $ parseStr getInput
 
 getInput :: String
-getInput = "(-10)"
+getInput = "(10 - 5)"
 
 parseStr :: String -> [String]
 parseStr str = parseStr' str [] where 
     parseStr' [] s = s
     parseStr' (x:xs) s 
         | isNumber x = 
+            -- собрать число
             let token = x : nextToken xs
                 start = length token - 1
                 end = length xs
@@ -27,13 +28,13 @@ parseStr str = parseStr' str [] where
         | isMaybeUnary x = 
             let prev = if length s > 0 then last s else ""
             in if isNumeric prev 
-                then parseStr' xs s
+                then parseStr' xs (s ++ [[x]])
                 else             
                     let token = x : nextToken xs
                         start = length token - 1
                         end = length xs
                     in parseStr' (substring start end xs) (s ++ [token])
-        | otherwise = parseStr' xs s
+        | otherwise = parseStr' xs (s ++ [[x]])
 
 nextToken :: String -> String
 nextToken [] = []
@@ -49,6 +50,7 @@ isNumeric [] = False
 isNumeric (x:xs) 
     | x == '-' || x == '+' = isNumeric xs
     | isNumber x = all isNumber xs
+    | otherwise = False
 
 substring :: Int -> Int -> String -> String
 substring start end str = take (end - start) (drop start str)
