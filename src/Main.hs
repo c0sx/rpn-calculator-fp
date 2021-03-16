22758c88c5d8fceda297dd21603014fce9bfb2ef
c0sx <vallkhale@gmail.com>
2021-03-16 17:40:22 +0300

    temp

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,54 @@
 module Main where
 
+import Data.Char(isNumber)
+
 import App.Cli.Input(welcome)
 import App.Cli.Output(showCalculation)
 import App.Calculator.Calculator(calculateFromString)
 
 main :: IO ()
-main = do
-    input <- welcome "Введите выражение:"
+main = 
+    -- input <- welcome "Введите выражение:"
 
-    showCalculation . calculateFromString $ input
+    print $ parseStr getInput
+
+getInput :: String
+getInput = "(-10)"
+
+parseStr :: String -> [String]
+parseStr str = parseStr' str [] where 
+    parseStr' [] s = s
+    parseStr' (x:xs) s 
+        | isNumber x = 
+            let token = x : nextToken xs
+                start = length token - 1
+                end = length xs
+            in parseStr' (substring start end xs) (s ++ [token])
+        | isMaybeUnary x = 
+            let prev = if length s > 0 then last s else ""
+            in if isNumeric prev 
+                then parseStr' xs s
+                else             
+                    let token = x : nextToken xs
+                        start = length token - 1
+                        end = length xs
+                    in parseStr' (substring start end xs) (s ++ [token])
+        | otherwise = parseStr' xs s
+
+nextToken :: String -> String
+nextToken [] = []
+nextToken (c:xs) 
+    | isNumber c = c : nextToken xs 
+    | otherwise = []
+
+isMaybeUnary :: Char -> Bool 
+isMaybeUnary x = x == '+' || x == '-'
+
+isNumeric :: String -> Bool
+isNumeric [] = False
+isNumeric (x:xs) 
+    | x == '-' || x == '+' = isNumeric xs
+    | isNumber x = all isNumber xs
+
+substring :: Int -> Int -> String -> String
+substring start end str = take (end - start) (drop start str)
